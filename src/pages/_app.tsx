4579122c1ea5da7df80fc5381794f29248fac78d
alexefthymioutes <alex.efthymiou@tes.com>
2023-11-29 11:48:18 +0000

    another redirect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React, {useEffect} from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    // Check if the current domain is seabrookfinance.co.uk
+    if (window.location.hostname === 'seabrookfinance.co.uk') {
+      // Redirect to seabrookfinance.com
+      window.location.replace('https://seabrookfinance.com' + router.asPath);
+    }
+  }, []);
   return <Component {...pageProps} />
 }
