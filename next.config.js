a105c3ca5d513fe32975beb3f70602af127e36e9
alexefthymioutes <alex.efthymiou@tes.com>
2023-11-29 11:37:24 +0000

    redo of redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    // Check if the environment is production
+    if (process.env.NODE_ENV === 'production') {
+      return [
+        {
+          source: '/:path*',
+          destination: 'https://seabrookfinance.com/:path*',
+          permanent: true,
+        },
+        // Additional redirect to handle root domain
+        {
+          source: '/',
+          destination: 'https://seabrookfinance.com/',
+          permanent: true,
+        },
+      ];
+    }
+
+    // Return an empty array for local development
+    return [];
+  },
 }
 
 module.exports = nextConfig
