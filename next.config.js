7ec9558f7892b7ec5b903de3a8ff58c3e82e751c
alexefthymioutes <alex.efthymiou@tes.com>
2023-11-29 11:26:43 +0000

    revert redirect

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    // Check if the environment is production
-    if (process.env.NODE_ENV === 'production') {
-      return [
-        {
-          source: '/:path*',
-          destination: 'https://seabrookfinance.com/:path*',
-          permanent: true,
-        },
-      ];
-    }
-
-    // Return an empty array for local development
-    return [];
-  },
 }
 
 module.exports = nextConfig
